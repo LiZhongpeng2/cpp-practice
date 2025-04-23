612bfb14e1d1b3ba5b4e78711dfce47b178f0a3a
LiZhongpeng2 <lizhongpeng2@gmail.com>
2025-04-23 11:26:46 +0800

    Friend Function

--- practice/friendFunction.cpp
+++ practice/friendFunction.cpp
@@ -1,5 +1,15 @@
 #include<iostream>
 
+/*
+Friend Function：
+1.突破类封装限制、允许外部函数访问私有成员的特权函数，它不属于类，却能进去你家翻抽屉
+2.类内定义，类外调用；
+3.友元函数是普通函数，不是类的成员；
+4.所以不需要对象调用，也没有 this 指针；
+5.但它可以访问类的私有和保护成员。
+-----------
+friend void showSecret(const A& a); // 声明友元
+*/
 class Girl {
     public:
         Girl(std::string n, int d){
