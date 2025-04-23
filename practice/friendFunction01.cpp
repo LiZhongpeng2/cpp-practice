3008313e14b0803f835fbd25b7f9db90272718a9
LiZhongpeng2 <lizhongpeng2@gmail.com>
2025-04-23 11:41:07 +0800

    friend function01

---
+++ practice/friendFunction01.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+
+/*
+a function can be defined as a frined function of two classes
+*/
+
+class Boy; //Advance Statement
+class Girl {
+    public:
+        Girl(std::string n, int d);
+        friend void disp(const Girl &, const Boy &);
+
+    private:
+        std::string name;
+        int age;
+};
+
+Girl::Girl(std::string n, int a){
+    name = n;
+    age = a;
+}
+
+class Boy{
+    public:
+        Boy(std::string n, int a);
+        friend void disp(const Girl& plg, const Boy& plb);
+
+    private:
+        std::string name;
+        int age;
+};
+
+Boy::Boy(std::string n, int a){
+    name = n;
+    age = a;
+}
+
+void disp(const Girl& plg, const Boy& plb){
+    std::cout << "The girl's name is " << plg.name << ", age is " << plg.age << std::endl;
+    std::cout << "The boy's name is " << plb.name << ", age is " << plb.age << std::endl;
+}
+
+int main(int argc, char const *argv[])
+{
+    Girl g1("Li Mei",18);
+    Boy b1("Wang Qiang",19);
+    
+    disp(g1,b1);
+    return 0;
+}
+
+
