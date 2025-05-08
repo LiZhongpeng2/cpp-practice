2d59ef6a29cd4b5d7b730af27ff8952656851022
LiZhongpeng2 <lizhongpeng2@gmail.com>
2025-05-08 09:52:33 +0800

    multiple inheritance rules

---
+++ practice/multiple_inheritance.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+
+//多继承使用规则
+//类的本质也是一种类型声明，需要加;
+
+class X{
+    public:
+        void setX(int x1){
+            a = x1;
+        }
+        void showX(){
+            std::cout << "a=" << a << std::endl;
+        }
+
+    private:
+        int a;
+};
+
+class Y{
+    public:
+        void setY(int y1){
+            b = y1;
+        }
+        void showY(){
+            std::cout << "b=" << b << std::endl;
+        }
+    private:
+        int b;
+};
+
+class Z:public X, private Y{ //多继承，单独声明继承类型，默认private: class Z:X,Y(private X, private Y)
+    public:
+        void setZ(int x, int y){
+            c = x;
+            setY(y);
+        }
+        void showZ(){
+            showY();
+            std::cout << "c=" << c << std::endl;
+        }
+    private:
+        int c;
+};
+
+int main(int argc, char const *argv[])
+{
+    Z obj;
+    obj.setX(3);
+    obj.showX();
+    obj.setZ(6, 8);
+    obj.showZ();
+    return 0;
+}
